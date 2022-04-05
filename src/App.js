2057efb5eb2e29b8edfdd0e88c8df4f956d94d26
Milind <milindkhariwale83@gmail.com>
2022-04-05 07:45:29 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
+import React from "react";
 import "./App.css";
+import Form from "./Components/Form";
 
 function App() {
-  return (
-    <div className="container">
-      <form>
-        <h1>Login Form</h1>
-        <div className="ui divider"></div>
-        <div className="ui form">
-          <div className="field">
-            <label>User Name: </label>
-            <input type="text " name="username" placeholder="Username" />
-          </div>
-          <div className="field">
-            <label> Email:</label>
-            <input type="email" name="email" placeholder="Email" />
-          </div>
-          <div className="field">
-            <label>Password:</label>
-            <input type="password" name="password" placeholder="Password" />
-          </div>
-          <button className="fluid ui button blue">Submit</button>
-        </div>
-      </form>
-    </div>
-  );
+  return <Form />;
 }
 
 export default App;
